eeb01ea9bc3ed06d51de9095f2290822289eb136
doniyorich <donish1979@gmail.com>
2026-01-06 17:22:40 +0300

    modified POST category endpoint

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select, update
+from sqlalchemy.orm import Session
+
+from app.models.categories import Category as CategoryModel
+from app.schemas import Category as CategorySchema, CategoryCreate
+from app.db_depends import get_db
 
 # Создаем маршрутизатор с префиксом и тэгом
 router = APIRouter(
@@ -15,12 +21,35 @@ async def get_all_categories():
     return {"message": "Список всех категорий (заглушка)"}
 
 
-@router.post("/")
-async def create_category():
+@router.post("/", response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
+async def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
     """
     Создает новую категорию.
     """
-    return {"message": "Категория создана (заглушка)"}
+    # Проверка существования parent_id, если указан
+    if category.parent_id is not None:
+        stmt = select(CategoryModel).where(
+            CategoryModel.id == category.parent_id,
+            CategoryModel.is_active == True)
+        parent = db.scalars(stmt).first()
+        if parent is None:
+            raise HTTPException(status_code=400, detail="Parent category not found")
+
+    # Создание новой категории
+    # преобразуем (model_dump) Pydantic модель category в словарь, распакуем параметры (**)
+    # и создадим SQLAlchemy модель db_category
+    db_category = CategoryModel(**category.model_dump())
+
+    # добавляем в сессию db, но пока НЕ записываем. Alchemy его отслеживает, как stage (индекс) в git
+    db.add(db_category)
+
+    # вот теперь идет запись в БД, а именно INSERT ....
+    db.commit()
+
+    # Обновляем саму модель Category в контексте Python, т.е. получаем например обновленный id.
+    db.refresh(db_category)
+
+    return db_category
 
 
 @router.put("/{category_id}")
