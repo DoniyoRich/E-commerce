e0d40857e03512425a5b82d3f4eae30f523dbd30
doniyorich <donish1979@gmail.com>
2026-01-10 16:55:16 +0300

    Project is set for async mechanism, POST endpoint finished

--- app/database.py
+++ app/database.py
@@ -10,6 +10,20 @@ engine = create_engine(DATABASE_URL, echo=True)
 # Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
 
+# ------------------ Асинхронное подключение к PostgreSQL --------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from constants import DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASS
+
+# Строка подключения для PostgreSQL
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Создаем Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
 
 # Определяем базовый класс для моделей
 class Base(DeclarativeBase):
